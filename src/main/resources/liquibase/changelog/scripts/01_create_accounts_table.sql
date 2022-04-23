cca978b0957477390375fdee78de12069dd8b1b9
Shyam Raj Prasad <shyamrajprasad@shyam.local>
2022-04-23 16:03:12 +0530

    mule spring liquibase integration

---
+++ src/main/resources/liquibase/changelog/scripts/01_create_accounts_table.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset shyam:1
+
+CREATE TABLE accounts (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP 
+);
